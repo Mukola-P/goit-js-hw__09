a377386518d5832e8cf7a496b9293dc8eb5ea888
Mukola-P <proxorov.mukola@gmail.com>
2022-10-10 16:28:07 +0300

    goit-js-hw__09-task__1-3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+startBtn.addEventListener('click', eventButtonHandler);
+
+const randomHexColor = getRandomHexColor();
+let timerId = null;
+
+function eventButtonHandler() {
+  timerId = setInterval(() => {
+    // startBtn.disabled = true;
+    const body = document.querySelector('body');
+    const color = getRandomHexColor();
+    body.style.backgroundColor = `${color}`;
+  }, 1000);
+  startBtn.disabled = true;
+}
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+});
